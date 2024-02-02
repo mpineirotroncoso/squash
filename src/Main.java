e619face5d8dc68c13c8324d447409ebd31809e1
Joel Pita Vidal <jpitavidal@danielcastelao.org>
2024-02-02 09:31:18 +0100

    cambio2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Holaa");
         System.out.println("Esto es un cambio");
+        System.out.println("Esot es otro cambio");
+
     }
 }
