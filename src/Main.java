0f292b24c5362f1cb8bb8932c4ba6dc98463a2f8
Marcos <mpineirotroncoso@danielcastelao.org>
2024-02-02 09:34:29 +0100

    squash

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Holaa");
+        System.out.println("Esto es un cambio");
+        System.out.println("Esot es otro cambio");
+        System.out.println("Esto es el ultimo cambio");
     }
 }
